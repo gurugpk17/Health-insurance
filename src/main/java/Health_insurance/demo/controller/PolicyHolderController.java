bd7e1349cdab80a3e58696c56858564674b3f765
gurugpk17 <gurumoorthy.2002@srit.org>
2025-01-19 19:42:34 +0530

    PolicyHolderController is changed

--- src/main/java/Health_insurance/demo/controller/PolicyHolderController.java
+++ src/main/java/Health_insurance/demo/controller/PolicyHolderController.java
@@ -1,8 +1,12 @@
 package Health_insurance.demo.controller;
 
+import Health_insurance.demo.dto.PolicyHolderDTO;
 import Health_insurance.demo.model.PolicyHolder;
 import Health_insurance.demo.services.PolicyHolderService;
+import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,15 +18,25 @@ public class PolicyHolderController {
     @Autowired
     private PolicyHolderService service;
 
+    // Create a new PolicyHolder
     @PostMapping
-    public PolicyHolder createPolicyHolder(@RequestBody PolicyHolder policyHolder) {
-        return service.createPolicyHolder(policyHolder);
+    public ResponseEntity<String> createPolicyHolder(@Valid @RequestBody PolicyHolderDTO policyHolderDTO) {
+        PolicyHolder policyHolder = new PolicyHolder();
+        policyHolder.setPolicyHolderName(policyHolderDTO.getPolicyHolderName());
+        policyHolder.setPolicyHolderDob(policyHolderDTO.getPolicyHolderDob());
+        policyHolder.setPolicyHolderGender(policyHolderDTO.getPolicyHolderGender());
+        policyHolder.setPolicyHolderAddress(policyHolderDTO.getPolicyHolderAddress());
+        policyHolder.setPolicyHolderPhNo(policyHolderDTO.getPolicyHolderPhNo());
+
+        // Save the PolicyHolder using the service
+        service.createPolicyHolder(policyHolder);
+
+        return ResponseEntity.ok("Policy Holder created successfully!");
     }
 
+    // Get all PolicyHolders
     @GetMapping
     public List<PolicyHolder> getAllPolicyHolders() {
         return service.getAllPolicyHolders();
     }
-
-
 }
